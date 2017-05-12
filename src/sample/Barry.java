5adf24800cc376ff8278250672ccdf33468e641a
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-05-12 07:44:47 -0700

    Adding a new Scientist class

--- src/sample/Barry.java
+++ src/sample/Barry.java
@@ -1,7 +1,5 @@
 package sample;
 
-import javafx.scene.image.Image;
-
 /**
  * Created by sguduguntla on 5/11/17.
  */
---
+++ src/sample/Scientist.java
@@ -0,0 +1,12 @@
+package sample;
+
+/**
+ * Created by sguduguntla on 5/12/17.
+ */
+public class Scientist extends Actor {
+
+    @Override
+    void act(long now) {
+
+    }
+}
