fb12eefa9052bcc13a0bd68fac459a051c3f215d
Yusuf Mostafa <ymostafa30@gmail.com>
2017-05-17 08:54:25 -0700

    Add Collision Checking
    Signed-off-by: Yusuf Mostafa <Electrex@users.noreply.github.com>

--- src/sample/Barry.java
+++ src/sample/Barry.java
@@ -3,11 +3,16 @@ package sample;
 public class Barry extends Actor {
 
     private boolean isFalling;
+    private boolean endGame;
 
     public Barry() {
         this.isFalling = false;
-
+        endGame = false;
         setDy(-4);
+    }
+
+    public boolean getEndGame(){
+        return endGame;
     }
 
     public void setFalling(boolean falling) {
@@ -20,6 +25,12 @@ public class Barry extends Actor {
 
     @Override
     public void act(long now) {
+        Actor zapper = getOneIntersectingObject(Zapper.class);
+        Actor missile = getOneIntersectingObject(Missile.class);
+        Actor coin = getOneIntersectingObject(Coin.class);
+        if (zapper != null || missile != null){
+            endGame = true;
+        }
 
         if (isFalling) {
             setDy(Math.abs(getDy()) * 1.05);
